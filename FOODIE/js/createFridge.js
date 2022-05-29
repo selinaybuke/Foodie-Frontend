8b605a823346436c101c3cbc140684c348153561
Aleyna Serbest <aleynaserbestt@outlook.com>
2022-05-29 19:31:02 +0300

    createfridge

---
+++ FOODIE/js/createFridge.js
@@ -0,0 +1,40 @@
+var app = angular.module('createFridge', [])
+
+app.controller('createFridgeController', ['$scope', function ($scope) {
+    $scope.fridgeName = ""
+    $scope.saveFridge = function () {
+        console.log($scope.fridgeName)
+
+        var data = JSON.stringify({
+            "name": $scope.fridgeName
+        });
+
+        var xhr = new XMLHttpRequest();
+
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+                const response = JSON.parse(this.response)
+                localStorage.setItem("fridgeId",response.id)
+                window.location.href = 'http://127.0.0.1:5501/FOODIE/HTML/fridgeDetail.html';
+            }
+        });
+
+        xhr.open("POST", "http://localhost:8080/api/fridge/create");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.send(data);
+
+
+
+
+
+    }
+
+
+
+
+
+
+}]);
--- FOODIE/js/register.js
+++ FOODIE/js/register.js
@@ -40,3 +40,5 @@ angular.module('RegisterApp', [])
 
 
 
+
+
