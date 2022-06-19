5842a41a5a9b7d3dde2028abcf2996d96618fe33
halilemirleroglu <emirlerogluhalil@gmail.com>
2022-06-19 20:22:39 +0300

    fridgeDetail

--- FOODIE/js/createFridge.js
+++ FOODIE/js/createFridge.js
@@ -16,8 +16,28 @@ app.controller('createFridgeController', ['$scope', function ($scope) {
             if (this.readyState === 4) {
                 console.log(this.responseText);
                 const response = JSON.parse(this.response)
-                localStorage.setItem("fridgeId",response.id)
-                window.location.href = 'http://127.0.0.1:5501/FOODIE/HTML/createFridgeDetail.html';
+                localStorage.setItem("fridgeId", response.id)
+                var data = JSON.stringify({
+                    "appUser": {
+                        "id": localStorage.getItem("userId")
+                    },
+                    "fridge": {
+                        "id": localStorage.getItem("fridgeId")
+                    },
+                    "status": true
+                });
+
+                var xhr = new XMLHttpRequest();
+                xhr.addEventListener("readystatechange", function () {
+                    if (this.readyState === 4) {
+                        window.location.href = 'http://127.0.0.1:5501/FOODIE/HTML/fridgeDetail.html';
+
+                    }
+                });
+                xhr.open("POST", "http://localhost:8080/api/fridge/createRelation");
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.send(data);
+
             }
         });
 
--- FOODIE/js/createFridgeDetail.js
+++ FOODIE/js/createFridgeDetail.js
@@ -1,16 +1,15 @@
-var app = angular.module('fridgeDetail', [])
+var app = angular.module('fridgeDetail', ['ngRoute', 'ui.bootstrap'])
 
 app.controller('fridgeInitController', ['$scope', function ($scope) {
     $scope.init = function () {
-
         var xhr = new XMLHttpRequest();
         xhr.addEventListener("readystatechange", function () {
             if (this.readyState === 4) {
                 const response = JSON.parse(this.response)
                 $scope.fridgeName = response;
-               // console.log($scope.fridgeName[0].fridge.name)
+                // console.log($scope.fridgeName[0].fridge.name)
                 $scope.$apply();
-                
+
             }
         });
         xhr.open("GET", "http://localhost:8080/api/fridge/getRelationByFridgeId?id=" + localStorage.getItem("fridgeId"));
@@ -22,3 +21,32 @@ app.controller('fridgeInitController', ['$scope', function ($scope) {
 
 
 }]);
+app.controller('ingredientController', function ($scope) {
+    $scope.ingredient = {
+        name: "",
+        readonly: true
+    };
+    $scope.getIngredient = function () {
+        var xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+                const response = JSON.parse(this.response)
+                $scope.ingredients = response;
+                $scope.$apply();
+            }
+        });
+
+        xhr.open("GET", "https://api.spoonacular.com/food/ingredients/autocomplete?query=" + $scope.ingredient.name + "&apiKey=c41e117a8ec343168d08c412fd210144");
+
+        xhr.send();
+    }
+    $scope.selectTypeAhead = function ($item) {
+
+        $scope.ingredient.name = $item.name;
+        console.log($scope.ingredient.name);
+
+    };
+
+});
